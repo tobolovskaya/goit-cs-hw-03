c941d55e42c96b6e2f1a528b9ceaf457ba19bd86
Yevheniia Tobolovska <yevheniia.tobolovska@nordsaltenkraft.no>
2025-07-31 09:54:08 +0200

    Create config.py

---
+++ task1/config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "qwerty12345",
+    "host": "localhost",
+    "port": "5432",
+}
+
+NUMBER_USERS = 10
+NUMBER_TASKS = 20
+STATUSES = ["new", "in progress", "completed"]
