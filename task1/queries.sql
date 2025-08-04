b8bad27a69bd02ff189a64ba171e10481a9f112f
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-08-04 08:41:17 +0200

    Update queries.sql

--- task1/queries.sql
+++ task1/queries.sql
@@ -8,10 +8,11 @@ SELECT * FROM tasks WHERE status_id = (SELECT id FROM status WHERE name = 'new')
 UPDATE tasks SET status_id = (SELECT id FROM status WHERE name = 'in progress') WHERE id = 1;
 
 -- Отримати список користувачів, які не мають жодного завдання
-SELECT * FROM users WHERE id NOT IN (SELECT user_id FROM tasks);
+SELECT * FROM users WHERE id NOT IN (SELECT DISTINCT user_id FROM tasks);
 
 -- Додати нове завдання для конкретного користувача
-INSERT INTO tasks (title, description, status_id, user_id) VALUES ('New Task', 'Description', 1, 2);
+INSERT INTO tasks (title, description, status_id, user_id)
+VALUES ('New Task', 'Description', (SELECT id FROM status WHERE name = 'new'), 2);
 
 -- Отримати всі завдання, які ще не завершено
 SELECT * FROM tasks WHERE status_id != (SELECT id FROM status WHERE name = 'completed');
@@ -26,13 +27,22 @@ SELECT * FROM users WHERE email LIKE '%@gmail.com';
 UPDATE users SET fullname = 'Updated Name' WHERE id = 1;
 
 -- Отримати кількість завдань для кожного статусу
-SELECT status_id, COUNT(*) FROM tasks GROUP BY status_id;
+SELECT s.name, COUNT(t.id) AS task_count
+FROM status s LEFT JOIN tasks t ON s.id = t.status_id
+GROUP BY s.name;
 
--- Отримати завдання, що не мають опису
+-- Отримати завдання, які призначені користувачам з певною доменною частиною електронної пошти
+SELECT t.* FROM tasks t JOIN users u ON t.user_id = u.id WHERE u.email LIKE '%@example.com';
+
+-- Отримати список завдань, що не мають опису
 SELECT * FROM tasks WHERE description IS NULL;
 
 -- Вибрати користувачів та їхні завдання у статусі 'in progress'
-SELECT u.fullname, t.title FROM users u INNER JOIN tasks t ON u.id = t.user_id WHERE t.status_id = (SELECT id FROM status WHERE name = 'in progress');
+SELECT u.fullname, t.title FROM users u
+JOIN tasks t ON u.id = t.user_id
+WHERE t.status_id = (SELECT id FROM status WHERE name = 'in progress');
 
 -- Отримати користувачів та кількість їхніх завдань
-SELECT u.fullname, COUNT(t.id) FROM users u LEFT JOIN tasks t ON u.id = t.user_id GROUP BY u.id;
+SELECT u.fullname, COUNT(t.id) AS task_count
+FROM users u LEFT JOIN tasks t ON u.id = t.user_id
+GROUP BY u.fullname;
